ddf18efa9e27a993af62bd96a6c870d1aa7a827e
helloworld <1334087806@qq.com>
2019-10-23 02:03:55 -0700

    week7

---
+++ week7/code/simplefork.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<sys/types.h>
+#include<unistd.h>
+#include<stdlib.h>
+
+int global = 4;
+
+int main(void)
+{
+	pid_t pid;
+	int vari = 5;
+	
+	printf("before fork\n");
+	if((pid = fork()) < 0)
+	{
+		printf("fork error\n");
+		exit(0);
+	}else if(pid == 0)
+	{
+		global++;
+		vari--;
+		printf("Child changed\n");
+		printf("child:global = %d vari = %d\n",global,vari);
+	}else
+	{
+		printf("Parents did not changed\n");
+		printf("parent:global = %d vari = %d\n",global,vari);
+		exit(0);
+	}
+	
+}
